65e03ba5e89384a009c18a97b8548d01dca7625b
DoheeKim <dhkim@ishift.co.kr>
2021-07-25 22:00:01 +0900

    Feat. UserPost model

---
+++ RxMvvmTableView/Models/UserPost.swift
@@ -0,0 +1,17 @@
+//
+//  UserPost.swift
+//  RxMvvmTableView
+//
+//  Created by 김도희 on 2021/07/25.
+//
+
+import Foundation
+
+class UserPost: Codable {
+
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+
+}
